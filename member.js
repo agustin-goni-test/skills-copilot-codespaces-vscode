a5bcedf4b8e39ece0c0458fd38c2b6d649f911d6
agustin-goni-test <117552271+agustin-goni-test@users.noreply.github.com>
2023-07-25 16:15:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMember()
+{
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    var homeContent = document.getElementById("homeContent");
+    member.style.color = "#f5f5f5";
+    skills.style.color = "#000000";
+    contact.style.color = "#000000";
+    about.style.color = "#000000";
+    home.style.color = "#000000";
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+    homeContent.style.display = "none";
+}
+
+
+
+
